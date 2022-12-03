cc8802be11e94c3f32d29a3608c87c7f2f559fa1
Uplabdhi Bhardwaj <rimbhard@usc.edu>
2022-12-03 01:40:11 +0000

    Initial commit for hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,5 +1,3 @@
-
-
 #include <set>
 #include <iostream>
 #include <fstream>
@@ -19,6 +17,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t day, size_t shift, size_t worker, size_t shiftCount, size_t workerCount);
+
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -35,9 +36,35 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, 0, 0, 0);
+
+
 
 
 
 
 }
 
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t day, size_t shift, size_t worker, size_t shiftCount, size_t workerCount){
+    if(day == avail.size()){
+        return true;
+    }
+    if(shift == dailyNeed){
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, day+1, 0, 0, 0, 0);
+    }
+    if(worker == avail[0].size()){
+        return false;
+    }
+    if(avail[day][worker] == true){
+        if(shiftCount < maxShifts){
+            if(workerCount < dailyNeed){
+                sched[day].push_back(worker);
+                if(scheduleHelper(avail, dailyNeed, maxShifts, sched, day, shift+1, 0, shiftCount+1, workerCount+1)){
+                    return true;
+                }
+                sched[day].pop_back();
+            }
+        }
+    }
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, day, shift, worker+1, shiftCount, workerCount);
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,3 +1,4 @@
+
 // For debugging
 #include <iostream>
 // For std::remove
@@ -10,16 +11,97 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void helper (const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& answers, std::string curr, std::map<char, int> floating_map);
 
 
+// Definition of primary wordle function
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    std::set<std::string> possible_words;
 
+    // Generate all possible combinations of fixed and floating letters
+    for (int i = 0; i < in.length(); i++) {
+        // Skip if the current position is not a blank space
+        if (in[i] != '-') continue;
+
+        // Replace the blank space with all possible letters of the alphabet
+        for (char c = 'a'; c <= 'z'; c++) {
+            std::string word = in;
+            word[i] = c;
+
+            // Add floating characters to the word in any remaining blank spaces
+            for (int j = 0; j < floating.length(); j++) {
+                for (int k = 0; k < word.length(); k++) {
+                    if (word[k] == '-') {
+                        word[k] = floating[j];
+                        break;
+                    }
+                }
+            }
+
+            // Check if the generated word is a valid English-language word
+            if (dict.find(word) != dict.end()) {
+                // Add the word to the set of possible words
+                possible_words.insert(word);
+            }
+        }
+    }
+
+    return possible_words;
 }
 
-// Define any helper functions here
+
+void helper (const std::string& in, const std::string& floating, const std::set<std::string>& dict, 
+std::set<std::string>& answers, std::string curr, std::map<char, int> floating_map) {
+    // base case
+    if (curr.size() == in.size()) {
+        if (dict.find(curr) != dict.end()) {
+            for(auto s : answers){
+                    cout << s << endl;
+                }
+            answers.insert(curr);
+        }
+        return;
+    }
+    // if the current character is not a dash, recurse to the next character
+    if (in[curr.size()] != '-') {
+        helper(in, floating, dict, answers, curr + in[curr.size()], floating_map);
+        return;
+    }
+    // if the floating map is empty, we can only use letters from the dictionary
+    if (floating_map.empty()) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            // check if the word is valid before recursing
+            if (dict.find(curr + c) != dict.end()) {
+                helper(in, floating, dict, answers, curr + c, floating_map);
+            }
+        }
+    }
+    // if the floating map is not empty, we can use letters from the floating map
+    else {
+        for (auto it = floating_map.begin(); it != floating_map.end(); it++) {
+            // if the floating map has a letter that is in the dictionary, we can use it
+            if (dict.find(curr + it->first) != dict.end()) {
+                helper(in, floating, dict, answers, curr + it->first, floating_map);
+            }
+            // if the floating map has a letter that is not in the dictionary, we can use it
+            else {
+                helper(in, floating, dict, answers, curr + it->first, floating_map);
+            }
+            // if the floating map has a letter that is in the dictionary, we can use it
+            if (it->second > 1) {
+                it->second -= 1;
+                // check if the word is valid before recursing
+                if (dict.find(curr + it->first) != dict.end()) {
+                    helper(in, floating, dict, answers, curr + it->first, floating_map);
+                }
+                it->second += 1;
+            }
+        }
+    }
+}
+
